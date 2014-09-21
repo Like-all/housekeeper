d395a66a954d9d3c9f0e9c3fde562cdd1aeb3c1c
Like-all <like-all@yandex.com>
2014-09-21 19:47:20 +0400

    debian installation rule

--- debian.go
+++ debian.go
@@ -6,7 +6,7 @@ import (
 )
 
 func apt(pkg string) () {
-    cmd := exec.Command("sudo", "apt-get", "install", pkg)
+    cmd := exec.Command("sudo", "apt-get", "-y", "--no-install-recommends", "install", pkg)
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
     cmd.Stdin = os.Stdin
